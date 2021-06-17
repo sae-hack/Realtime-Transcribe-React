442be3ba50fce9f9e3e789f30263a2ca74a932ec
Sam Liu <xinhol@amazon.com>
2021-06-16 19:51:59 -0700

    Add spinner

--- src/RecordControl.tsx
+++ src/RecordControl.tsx
@@ -1,26 +1,35 @@
 import { AmplifyButton } from "@aws-amplify/ui-react";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
+import { ClipLoader } from "react-spinners";
 import styled from "styled-components";
 import { useUser } from "./hooks";
 import { makeRequest } from "./utils";
 
 const Styles = styled.div`
+  display: flex;
   width: auto;
+  align-items: center;
 `;
 
 const RecordControl: React.FC = () => {
   const user = useUser();
+  const [loading, setLoading] = useState(false);
 
   const handleStartSession = useCallback(() => {
     if (user) {
-      makeRequest("/start-session").then((data) => {
-        console.log(data);
-      });
+      setLoading(true);
+      makeRequest("/start-session")
+        .then((data) => {
+          console.log(data);
+          setLoading(false);
+        })
+        .catch(() => setLoading(false));
     }
   }, [user]);
 
   return (
     <Styles>
+      {loading && <ClipLoader color="white" size={20} />}
       <AmplifyButton disabled={!user} onClick={handleStartSession}>
         Start
       </AmplifyButton>
