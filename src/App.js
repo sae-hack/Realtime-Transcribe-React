db4358572ef68fb4d424ee5c0fb65461ada98df3
Sam Liu <xinhol@amazon.com>
2021-06-16 18:33:01 -0700

    Run prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { API, Auth } from "aws-amplify";
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 const App = () => {
   useEffect(() => {
-
-    Auth.currentSession().then(session => {
+    Auth.currentSession().then((session) => {
       const jwtToken = session.getIdToken().getJwtToken();
-      console.log('jwtToken', jwtToken);
+      console.log("jwtToken", jwtToken);
 
-      API.get('SAMAPI', '/start-session', { headers: { "Authorization": "Bearer " + jwtToken } }).then((data) => console.log(data)).catch((error) => console.log(error))
-    })
-  }, [])
+      API.get("SAMAPI", "/start-session", {
+        headers: { Authorization: "Bearer " + jwtToken },
+      })
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error));
+    });
+  }, []);
 
-  return <AmplifyAuthenticator>
-    <div>
-      My App
-      <AmplifySignOut />
-    </div>
-  </AmplifyAuthenticator>
+  return (
+    <AmplifyAuthenticator>
+      <div>
+        My App
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
+  );
 };
 
 export default App;
