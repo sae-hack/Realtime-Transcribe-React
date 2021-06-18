7ac81aa9b8ff7f04a165d8b53c56c15a1fd13291
Sam Liu <xinhol@amazon.com>
2021-06-18 01:42:49 -0700

    Add icons to indicate texts/speakers are editable

--- src/EditableMarkdown.tsx
+++ src/EditableMarkdown.tsx
@@ -1,6 +1,24 @@
 import React, { useState } from "react";
 import MarkdownEditor, { MarkdownEditorProps } from "./MarkdownEditor";
 import ReactMarkdown from "react-markdown";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const EditorContainer = styled.div`
+  padding-bottom: 10px;
+`;
+
+const SpanContainer = styled.div`
+  min-height: 60px;
+  cursor: pointer;
+
+  svg {
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+  }
+`;
 
 const EditableMarkdown: React.FC<MarkdownEditorProps> = ({
   value,
@@ -9,15 +27,20 @@ const EditableMarkdown: React.FC<MarkdownEditorProps> = ({
   const [editing, setEditing] = useState(false);
 
   return editing ? (
-    <MarkdownEditor
-      value={value}
-      onChange={onChange}
-      onBlur={() => setEditing(false)}
-    />
+    <EditorContainer>
+      <MarkdownEditor
+        value={value}
+        onChange={onChange}
+        onBlur={() => setEditing(false)}
+      />
+    </EditorContainer>
   ) : (
-    <span onClick={() => setEditing(true)}>
-      <ReactMarkdown>{value}</ReactMarkdown>
-    </span>
+    <SpanContainer onClick={() => setEditing(true)}>
+      <span>
+        <ReactMarkdown>{value}</ReactMarkdown>
+      </span>
+      <FontAwesomeIcon icon={faEdit} />
+    </SpanContainer>
   );
 };
 
--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,5 +1,12 @@
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import styled from "styled-components";
+
+const Span = styled.span`
+  cursor: pointer;
+`;
 
 interface Props {
   children: string;
@@ -35,7 +42,9 @@ const EditableSpan: React.FC<Props> = ({ children, onChange }) => {
       </Button>
     </div>
   ) : (
-    <span onClick={() => setEditing(true)}>{value}</span>
+    <Span onClick={() => setEditing(true)}>
+      <FontAwesomeIcon icon={faEdit} /> {value}
+    </Span>
   );
 };
 
